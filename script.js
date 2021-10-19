ee513b7ffb5056229818997bb6b97c49dd9b741e
samilm4 <91291453+SamiLM4@users.noreply.github.com>
2021-10-19 16:22:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,10 @@ function Chutar() {
   var numeropensadoMaquina = parseInt(Math.random() * 3);
   console.log(numeropensadoMaquina);
   if (numeropensadoMaquina == 1) {
-    elementoTela.innerHTML =
-      " Faça o valor pensado + 5 - 2 + 7 + 4 + 8 - 5 + 3 - o valor inserido, o resultado deverá ser = 20 ";
+    elementoTela.innerHTML = (" Faça o valor pensado + 5 - 2 + 7 + 4 + 8 - 5 + 3 - o valor inserido, o resultado deverá ser = 20 ");
   } else if (numeropensadoMaquina == 2) {
-    elementoTela.innerHTML =
-      "Faça o valor pensado + 250 - 7 + 3 + 4 - 79 x 2 - o valor inserido, o resultado deverá ser = 342 ";
+    elementoTela.innerHTML = ("Faça o valor pensado + 250 - 7 + 3 + 4 - 79 x 2 - o valor inserido, o resultado deverá ser = 342 ");
   } else if (numeropensadoMaquina == 0) {
-    elementoTela.innerHTML =
-      "Faça o valor pensado - 7  x 5 + 70 : 7, - o valor inserido, o resultado deverá ser = 5";
+    elementoTela.innerHTML = ("Faça o valor pensado - 7  x 5 + 70 : 7, - o valor inserido, o resultado deverá ser = 5");
   }
 }
